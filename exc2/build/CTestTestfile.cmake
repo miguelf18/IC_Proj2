03cfe4e316736a5cec995120231c861043187c77
João Diogo Videira Oliveira <jdvoliveira@ua.pt>
2021-12-29 23:10:16 +0000

    update Parte C

--- exc2/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/DiogoLaptop/OneDrive - Universidade de Aveiro/4ano/IC/Labs/Project 02/exc2
-# Build directory: C:/Users/DiogoLaptop/OneDrive - Universidade de Aveiro/4ano/IC/Labs/Project 02/exc2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
